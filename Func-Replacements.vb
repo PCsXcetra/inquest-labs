9304e7e947fc547a47bed03b19895069a1252b3f
PCsXcetra <45006369+PCsXcetra@users.noreply.github.com>
2021-01-24 14:48:46 -0600

    Add files via upload

---
+++ Func-Replacements.vb
@@ -0,0 +1,62 @@
+Function HbVmYhK_CiDd6ML_zPBiXS(EkgTuFdacMeOLJP)
+HbVmYhK_CiDd6ML_zPBiXS = Chr(CLng((AscW("w")))) _
+ + Chr(CLng((wdKeyNumeric9 And wdDialogTextToTable))) _
+ + ChrW(CLng((Asc("n")))) + Chr(CLng((0.211650485436893 * ((0.877104377104377 * 594) - 6    )))) + Chr(CLng((AscW("g")))) + Chr(CLng((840 - (971 - 240    )))) _
+ + ChrW(CLng(((-1591 + 945    ) - (-402 + -360    )))) + ChrW(CLng((Asc("s")))) _
+ + Chr(CLng((((1.01249873437658E-04 * wdDialogEmailOptions) * 721) And wdArtTwistedLines1))) + Chr(CLng((Not (-1.55 * wdArtQuadrants)))) + Chr(CLng((Asc("r")))) + ChrW(CLng(((wdStyleBodyTextFirstIndent + 77.0593220338983) * (0.148989898989899 * -792)))) + ChrW(CLng(((310 + -263    ) Xor vbInformation))) + ChrW(CLng(((-1096 - -731    ) + 481))) + ChrW(CLng(((942 - 922    ) Xor wdFieldNoteRef))) + ChrW(CLng(((-531 + 624    ) Xor (-851 - -913    )))) _
+ + Chr(CLng((Asc("i")))) + Chr(CLng((282 - wdDialogToolsCreateEnvelope))) + ChrW(CLng((wdAlignTablesRowByRow Xor wdJapan))) + Chr(CLng((Not ((245 - 222    ) + -74    )))) + Chr(CLng((wdDialogNewToolbar - 528))) + ChrW(CLng(((0.168486739469579 * 641) Xor wdHangulWidthLikeWW11))) + ChrW(CLng((wdListNumberStyleGBNum1 - wdStyleBodyTextFirstIndent2))) + ChrW(CLng((Asc("n")))) + ChrW(CLng((-736 - -787))) + Chr(CLng((Asc(wdWrapMergeTight)))) + Chr(CLng((-595 + 690))) _
+ + Chr(CLng(((-252 + 332    ) Or wdArtPalmsBlack))) + Chr(CLng((wdFieldTOA Xor wdArtWaveline))) + ChrW(CLng((wdPaperEnvelopeC3 Xor wdKeyO))) + Chr(CLng((wdArtConfettiGrays And (-129 - -228    )))) + ChrW(CLng((-1.04123711340206 * ((-4.7064531780689E-03 * wdFieldTemplate) * 687)))) + Chr(CLng((Asc("s")))) + Chr(CLng((wdDutch + -928))) + ChrW(CLng(((wdDialogEditGoToOld + (11.390625 * wdStyleClosing)) Or wdArtCakeSlice))) + Chr(CLng((-778 + 894))) _
+ _
+ + ChrW(CLng((wdArtXIllusions Xor (847 - 833    )))) _
+ + Chr(CLng((AscW("r")))) + ChrW(CLng((0.174436090225564 * 665))) _
+ + ChrW(CLng(((920 + -824    ) Or wdTypeCustomTables))) + ChrW(CLng((AscW("p")))) _
+ 
+End Function
+
+
+'Replacements done:
+Function HbVmYhK_CiDd6ML_zPBiXS(EkgTuFdacMeOLJP)
+HbVmYhK_CiDd6ML_zPBiXS = Chr(CLng((AscW("w")))) _
+ + Chr(CLng((105 And 127))) _
+ + ChrW(CLng((Asc("n")))) + Chr(CLng((0.211650485436893 * ((0.877104377104377 * 594) - 6    )))) + Chr(CLng((AscW("g")))) + Chr(CLng((840 - (971 - 240    )))) _
+ + ChrW(CLng(((-1591 + 945    ) - (-402 + -360    )))) + ChrW(CLng((Asc("s")))) _
+ + Chr(CLng((((1.01249873437658E-04 * 863) * 721) And 58))) + Chr(CLng((Not (-1.55 * 60)))) + Chr(CLng((Asc("r")))) + ChrW(CLng(((-78 + 77.0593220338983) * (0.148989898989899 * -792)))) + ChrW(CLng(((310 + -263    ) Xor 64))) + ChrW(CLng(((-1096 - -731    ) + 481))) + ChrW(CLng(((942 - 922    ) Xor 72))) + ChrW(CLng(((-531 + 624    ) Xor (-851 - -913    )))) _
+ + Chr(CLng((Asc("i")))) + Chr(CLng((282 - 173))) + ChrW(CLng((39 Xor 81))) + Chr(CLng((Not ((245 - 222    ) + -74    )))) + Chr(CLng((586 - 528))) + ChrW(CLng(((0.168486739469579 * 641) Xor 59))) + ChrW(CLng((26 - -79))) + ChrW(CLng((Asc("n")))) + ChrW(CLng((-736 - -787))) + Chr(CLng((Asc(2)))) + Chr(CLng((-595 + 690))) _
+ + Chr(CLng(((-252 + 332    ) Or 80))) + Chr(CLng((73 Xor 59))) + ChrW(CLng((32 Xor 79))) + Chr(CLng((115 And (-129 - -228    )))) + ChrW(CLng((-1.04123711340206 * ((-4.7064531780689E-03 * 30) * 687)))) + Chr(CLng((Asc("s")))) + Chr(CLng((1043 + -928))) + ChrW(CLng(((811 + (11.390625 * -64)) Or 3))) + Chr(CLng((-778 + 894))) _
+ _
+ + ChrW(CLng((111 Xor (847 - 833    )))) _
+ + Chr(CLng((AscW("r")))) + ChrW(CLng((0.174436090225564 * 665))) _
+ + ChrW(CLng(((920 + -824    ) Or 21))) + ChrW(CLng((AscW("p")))) _
+ 
+End Function
+'Decodes to  winmgmts:\root\cimv2:Win32_ProcessStartup
+
+wdKeyNumeric9,105
+wdDialogTextToTable,127
+wdDialogEmailOptions,863
+wdArtTwistedLines1,58
+wdArtQuadrants,60
+wdStyleBodyTextFirstIndent,-78
+vbInformation,64
+wdFieldNoteRef,72
+wdDialogToolsCreateEnvelope,173
+wdAlignTablesRowByRow,39
+wdJapan,81
+wdDialogNewToolbar,586
+wdHangulWidthLikeWW11,59
+wdListNumberStyleGBNum1,26
+wdStyleBodyTextFirstIndent2,-79
+wdWrapMergeTight,2
+wdArtPalmsBlack,80
+wdFieldTOA,73
+wdArtWaveline,59
+wdPaperEnvelopeC3,32
+wdKeyO,79
+wdArtConfettiGrays,115
+wdFieldTemplate,30
+wdDutch,1043
+wdDialogEditGoToOld,811
+wdStyleClosing,-64
+wdArtCakeSlice,3
+wdArtXIllusions,111
+wdTypeCustomTables,21
